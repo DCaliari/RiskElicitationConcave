5d98efb4179f154962b647e5a69bce3b93681b49
DCaliari <68252050+DCaliari@users.noreply.github.com>
2026-04-27 10:46:24 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -27,6 +27,14 @@ SESSION_CONFIGS = [
     ),
 ]
 
+ROOMS = [
+    dict(
+        name='lab',
+        display_name='Lab',
+        participant_label_file='_rooms/lab.txt'
+    )
+]
+
 # if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
 # in SESSION_CONFIGS, except those that explicitly override it.
 # the session config can be accessed from methods in your apps as self.session.config,
